a9849ae2c0fe55bf2fd25026793079246a7f046a
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-12-22 12:48:22 +0100

    Created pokemon object and creating elements from it

--- src/home.ts
+++ src/home.ts
@@ -1,1 +1,109 @@
 import { Utils } from "./utils.js";
+
+type PokemonGender = 'male' | 'female';
+
+interface Pokemon {
+  id: number;
+  gender: PokemonGender;
+  name: string;
+  level: number;
+  life: {
+    current: number;
+    total: number;
+  }
+}
+
+const ennemyPokemon: Pokemon = {
+  gender: 'male',
+  name: 'Pikachu',
+  level: 5,
+  id: 25,
+  life: {
+    current: 20,
+    total: 30,
+  }
+}
+
+const playerPokemon: Pokemon = {
+  gender: 'female',
+  name: 'Bulbizarre',
+  level: 1,
+  id: 1,
+  life: {
+    current: 10,
+    total: 19,
+  }
+}
+
+function getPokemonImgFromId(id: number, isBack: boolean) {
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon${isBack ? '/back' : ''}/${id}.png`;
+}
+
+function createPokemonAndInfoBox(isEnnemy: boolean, pokemon: Pokemon) {
+  let container: HTMLElement = document.querySelector(isEnnemy ? 'header' : 'footer')!;
+
+  const classes = ['pokemon-and-info-container'];
+  classes.push(isEnnemy? 'ennemy-pokemon-and-info-container' : 'player-pokemon-and-info-container');
+  const pokemonInfoContainer = Utils.createNewElement('div', classes, container, null, false);
+  createPokemonInfoBox(isEnnemy, pokemon, pokemonInfoContainer);
+  createPokemonContainer(getPokemonImgFromId(pokemon.id, !isEnnemy), pokemonInfoContainer, isEnnemy);
+}
+
+function createPokemonContainer(imgUrl: string, container: HTMLElement, appendToContainer = true) {
+  const pokemonContainer = Utils.createNewElement('div', ['pokemon-container'], container, null, appendToContainer);
+  const pokemonImg = Utils.createNewElement('img', ['pokemon-img'], pokemonContainer);
+  pokemonImg.setAttribute('src', imgUrl);
+  pokemonImg.setAttribute('alt', 'pokemon');
+}
+
+function createPokemonInfoBox(isEnnemy: boolean, pokemon: Pokemon, container: HTMLElement) {
+  const pokemonInfoClasses = ['pokemon-info'];
+  pokemonInfoClasses.push(isEnnemy? 'ennemy-pokemon-info' : 'player-pokemon-info');
+  const pokemonInfo = Utils.createNewElement('div', pokemonInfoClasses, container);
+
+  const pokemonInfoHeader = Utils.createNewElement('div', ['pokemon-info-header'], pokemonInfo);
+  const pokemonTitle = Utils.createNewElement('div', ['pokemon-title'], pokemonInfoHeader);
+  const pokemonName = Utils.createNewElement('div', ['pokemon-name'], pokemonTitle, pokemon.name);
+
+  const genderElt = createGenderSVG(pokemon.gender === 'male');
+  pokemonTitle.appendChild(genderElt);
+
+  const pokemonLevelContainer = Utils.createNewElement('div', ['pokemon-level-container'], pokemonInfoHeader);
+  pokemonLevelContainer.textContent = `lvl ${pokemon.level}`;
+
+  const pokemonLifeInfo = Utils.createNewElement('div', ['pokemon-life-info'], pokemonInfo);
+  const pokemonLifePointsContainer = Utils.createNewElement('div', ['pokemon-life-points-container'], pokemonLifeInfo);
+  
+  const pokemonLifePoints = Utils.createNewElement('span', ['pokemon-life-points'], pokemonLifePointsContainer, pokemon.life.current.toString());
+  const pokemonLifePointsSeparator = Utils.createNewElement('span', ['pokemon-life-points-separator'], pokemonLifePointsContainer, ' / ');
+  const pokemonLifePointsTotal = Utils.createNewElement('span', ['pokemon-life-points-total'], pokemonLifePointsContainer, pokemon.life.total.toString());
+
+  const pokemonLifeBarContainer = Utils.createNewElement('div', ['pokemon-life-bar-container'], pokemonLifeInfo);
+  const pokemonLifeBar = Utils.createNewElement('div', ['pokemon-life-bar'], pokemonLifeBarContainer);
+  const pokemonLifePercent = pokemon.life.current / pokemon.life.total * 100;
+  pokemonLifeBar.style.width = pokemonLifePercent + '%';
+}
+
+function createGenderSVG(isMale: boolean = true) {
+  const svgElt = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svgElt.classList.add('pokemon-gender');
+  svgElt.classList.add(isMale ? 'pokemon-gender-male' : 'pokemon-gender-female');
+  svgElt.setAttribute('width', '24');
+  svgElt.setAttribute('height', '24');
+  svgElt.setAttribute('viewBox', '0 0 24 24');
+
+  const pathElt = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  if (isMale) {
+    pathElt.setAttribute('d', 'M21 9c0-4.97-4.03-9-9-9s-9 4.03-9 9c0 4.632 3.501 8.443 8 8.941v2.059h-3v2h3v2h2v-2h3v-2h-3v-2.059c4.499-.498 8-4.309 8-8.941zm-16 0c0-3.86 3.14-7 7-7s7 3.14 7 7-3.14 7-7 7-7-3.14-7-7z');
+  } else {
+    pathElt.setAttribute('d', 'M16 2v2h3.586l-3.972 3.972c-1.54-1.231-3.489-1.972-5.614-1.972-4.97 0-9 4.03-9 9s4.03 9 9 9 9-4.03 9-9c0-2.125-.741-4.074-1.972-5.614l3.972-3.972v3.586h2v-7h-7zm-6 20c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7z');
+  }
+  
+  svgElt.appendChild(pathElt);
+  return svgElt;
+}
+
+createPokemonAndInfoBox(true, ennemyPokemon);
+
+createPokemonAndInfoBox(false, playerPokemon);
+
--- src/utils.ts
+++ src/utils.ts
@@ -1,88 +1,12 @@
 export class Utils {
-  static createSimpleCard(name: string) {
-    const cards: HTMLElement = document.querySelector(".cards")!;
-
-    const card = Utils.createNewElement('div', 'card', cards);
-    const cardBody = Utils.createNewElement('div', 'card-body', card);
-    Utils.createNewElement('h2', 'card-title', cardBody, name);
-  }
-
-  static createCard(name: string, imgUrl: string) {
-    const cards: HTMLElement = document.querySelector(".cards")!;
-
-    const card = Utils.createNewElement('div', 'card', cards);
-    const cardHeader = Utils.createNewElement('div', 'card-header', card);
-
-    const cardImg = Utils.createNewElement('div', 'card-img', cardHeader);
-    cardImg.style.backgroundImage = `url(${imgUrl})`;
-
-    const cardBody = Utils.createNewElement('div', 'card-body', card);
-    Utils.createNewElement('h2', 'card-title', cardBody, name);
-  }
-
-  static createBoulderGradeTitle(title: string) {
-    const cards: HTMLElement = document.querySelector(".cards")!;
-    Utils.createNewElement('h3', 'boulder-grade-title', cards, title);
-  }
-
-  static createBoulderCard(name: string, grade: string, description?: string, imgUrl?: string) {
-    const cards: HTMLElement = document.querySelector(".cards")!;
-
-    const card = Utils.createNewElement('div', 'card', cards);
-    const cardHeader = Utils.createNewElement('div', 'card-header', card);
-
-    if (imgUrl) {
-      const cardImg = Utils.createNewElement('div', 'card-img', cardHeader);
-      cardImg.style.backgroundImage = `url(${imgUrl})`;
-    }
-
-    const cardBody = Utils.createNewElement('div', 'card-body', card);
-    Utils.createNewElement('h2', 'card-title', cardBody, `${name} (${grade})`);
-
-    Utils.createNewElement('p', 'card-description', cardBody, description);
-  }
-
-  private static createNewElement(eltName: string, className: string, parent: HTMLElement, innerHTML?: string) {
+  static createNewElement(eltName: string, classNames: string[], parent: HTMLElement, innerHTML: string | null = null, append = true) {
     const elt = document.createElement(eltName);
-    elt.classList.add(className);
+    classNames.forEach((className) => {
+      elt.classList.add(className);
+    });
+    
     if (innerHTML) elt.innerHTML = innerHTML;
-    parent.appendChild(elt);
+    append ? parent.appendChild(elt) : parent.prepend(elt);
     return elt;
-  }
-
-  private static createTitle(currentExoIndex: number) {
-    const title: HTMLElement = document.querySelector("title")!;
-    const h1: HTMLElement = document.querySelector("h1")!;
-
-    const text = currentExoIndex === 0 ? "Home" : `Exercice ${currentExoIndex}`;
-
-    title.textContent = text;
-    h1.textContent = text;
-  }
-
-  static createDescription(description: string, align?: string) {
-    const descr: HTMLElement = document.querySelector(".description")!;
-    descr.innerHTML = description;
-    if (align) descr.style.textAlign = align;
-  }
-
-  static initPage(currentExoIndex: number) {
-    Utils.createMenu(currentExoIndex);
-    Utils.createTitle(currentExoIndex);
-  }
-
-  private static createMenu(currentExoIndex: number) {
-    const nbExercices = 8;
-    const header: HTMLElement = document.querySelector("header")!;
-
-    const list = Utils.createNewElement('ul', 'menu', header);
-
-    for (let i = 1; i <= nbExercices; i++) {
-      const listItem = Utils.createNewElement('li', 'menu-item', list);
-      if (currentExoIndex === i) listItem.classList.add("selected");
-
-      const link = Utils.createNewElement('a', 'menu-link', listItem, `exo${i}`) as HTMLAnchorElement;
-      link.href = `exo${i}.html`;
-    }
   }
 }
